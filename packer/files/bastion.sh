f865f6f84ab79bc44f5c9d6e3bb888708c6c5ee4
Alan Christie <alan.christie@matildapeak.com>
2018-11-29 12:11:54 +0000

    - Creation of Bastion image
    - Adds bastion packer (actually now yacker files)
    - Smaller site.yaml for new bastion (this will soon go)

---
+++ packer/files/bastion.sh
@@ -0,0 +1,5 @@
+# An okd-orchestrator Packer File
+#
+# Add (prefix) CentOS user's PATH with $HOME/bin
+# Needs prefixing because of potential clash with pre-existing packer binary
+export PATH=$HOME/bin:$PATH

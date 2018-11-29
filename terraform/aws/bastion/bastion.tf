9ea709d6b1d545539c28877646cb04a55509e896
Alan Christie <alan.christie@matildapeak.com>
2018-11-29 12:19:31 +0000

    - Fixed bastion machine image

--- terraform/aws/bastion/bastion.tf
+++ terraform/aws/bastion/bastion.tf
@@ -3,7 +3,7 @@
 # -----------------------------------------------------------------------------
 
 resource "aws_instance" "bastion" {
-  ami = "${var.node_ami}"
+  ami = "${var.bastion_ami}"
   instance_type = "${var.bastion_image_type}"
   key_name = "${var.keypair_name}"
   vpc_security_group_ids = ["${aws_security_group.ssh.id}",

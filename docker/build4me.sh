68d47997317eac7b8a9e41640cc058c84b0405cb
Alan Christie <alan.christie@matildapeak.com>
2018-12-10 15:25:12 +0000

    - Adds build4me - Adds group creation for okdo account

---
+++ docker/build4me.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Builds a 'stable' docker image using your user and group IDs.
+# Required on some systems in order to properly handle permissions
+# in the volume mounted ion the OKD working directory.
+IMAGE_TAG=stable docker-compose build \
+    --build-arg uid=$(id -u) \
+    --build-arg gid=$(id -g)

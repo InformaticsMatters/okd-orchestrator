1abcab37dea93249682d1e8e44c45b8f55d91017
Alan Christie <alan.christie@matildapeak.com>
2019-03-27 08:38:38 +0000

    - Fixes issue #43   docker4me now just uses docker

--- docker4me.sh
+++ docker4me.sh
@@ -3,6 +3,8 @@
 # Builds a 'stable' docker image using your user and group IDs.
 # Required on some systems in order to properly handle permissions
 # in the volume mounted as the OKD working directory.
-IMAGE_TAG=stable docker-compose build \
+docker build \
     --build-arg uid=$(id -u) \
-    --build-arg gid=$(id -g)
+    --build-arg gid=$(id -g) \
+    --tag informaticsmatters/okd-orchestrator:stable \
+    .
